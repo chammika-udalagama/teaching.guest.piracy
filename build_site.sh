3a5397a8eaca5ae2a12356979cf3edba3c41a966
Chammika Udalagama <chammika@nus.edu.sg>
2021-09-29 10:59:42 +0800

    Automatic Commit to main at 2021-09-29 10:59:42

--- build_site.sh
+++ build_site.sh
@@ -8,7 +8,7 @@ R -e "rmarkdown::clean_site()"
 
 R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render('webpage.Rmd',output_file = 'index.html',output_dir = './docs')"
 
-cp -r pdf-files ./docs
+cp -r files ./docs
 
 #R -e "bookdown::bs4_book(theme = bookdown::bs4_book_theme(), repo = NULL,  lib_dir = "libs", pandoc_args = NULL, extra_dependencies = NULL,   split_bib = FALSE)"
 
